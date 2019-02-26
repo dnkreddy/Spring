3d07a5f23ce0a35d142b403a46d6297bd84955cd
NaveenReddy <40067241+dnkreddy@users.noreply.github.com>
2019-02-26 00:07:28 -0500

    Add files via upload

---
+++ SpringSetterIOCXML1/bin/com/lavi/tutorial/readme.txt
@@ -0,0 +1,4 @@
+IOC with XML configuration:Setter based injection 
+This class is Demo for Basic SpringContainer(BeanFactory)
+which may be depracated in future versions.
+Adding spring core,beans and context,commons-logging.jar files
---
+++ SpringSetterIOCXML1/src/com/lavi/tutorial/TestApp.java
@@ -0,0 +1,28 @@
+package com.lavi.tutorial;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * 
+ * @author LaVi Technologies
+ *
+ */
+public class TestApp {
+	public static void main(String[] args) {
+		// Basic Spring Container
+		/*BeanFactory factory = new XmlBeanFactory(new FileSystemResource(
+				"src/applicationContext.xml"));*/
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource(
+				"applicationContext.xml"));
+		
+		/*Person person = (Person) factory.getBean("person");
+		System.out.println("personName:"+person.getPersonName());
+		System.out.println("personAge:"+person.getAge());
+		System.out.println(person.getAddress().getCity());
+		System.out.println(person.getAddress().getPincode());*/
+		
+		
+		
+	}
+}
